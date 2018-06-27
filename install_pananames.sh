91d529c7d4fedca319dd05488ce205a0f8bb3301
pvy <pvy@fozzy.com>
2018-06-27 16:30:27 +0300

    Update deployer

--- install_pananames.sh
+++ install_pananames.sh
@@ -2,7 +2,7 @@
 
 cd /tmp
 yum -y install php php-mysqli
-wget https://codeload.github.com/Pananames/billmanager/zip/masterv
+wget https://codeload.github.com/Pananames/billmanager/tar.gz/master
 tar -xvf master --strip-components=1 -C /usr/local/mgr5/
 chmod +x /usr/local/mgr5/processing/pmpananames.php
 rm /usr/local/mgr5/README.md
